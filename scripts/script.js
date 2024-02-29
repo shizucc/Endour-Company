53d461ed6a5a01b1938ee3ad5fec955897ad687d
shizucc <syifanaalfi@gmail.com>
2024-02-29 13:42:50 +0700

    try merge intro

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
+function product_carousel() {
+  let products = document.getElementsByClassName("products");
+  let products_img = document.getElementsByClassName("product-carousel");
+
+  for (let product of products) {
+    product.style.display = "none";
+  }
+
+  for (let product_img of products_img) {
+    product_img.style.display = "none";
+  }
+
+  products[product_index].style.display = "inline";
+  products_img[product_img_index].style.display = "block";
+  product_index++;
+  if (product_index >= products.length) product_index = 0;
+  product_img_index++;
+  if (product_img_index >= products_img.length) product_img_index = 0;
+  setTimeout(product_carousel, 2500);
+}
+
+
 function insertServiceToDOM(serviceList) {
   const services = document.getElementById("services");
 
@@ -92,4 +114,7 @@ function insertServiceToDOM(serviceList) {
 
 document.addEventListener("DOMContentLoaded", () => {
   insertServiceToDOM(SERVICES);
+  var product_index = 0
+  var product_img_index = 0
+  product_carousel()
 });
